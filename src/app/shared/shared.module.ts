cc54d79f680da20e1276d281fd40426e1b598364
Juan M Scaparro <juanscaparro@gmail.com>
2024-07-23 17:21:36 +0200

    create routes and lazy load

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import { HomePagesComponent } from './pages/home-pages/home-pages.component';
+import { AboutPagesComponent } from './pages/about-pages/about-pages.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
+import { ContactPagesComponent } from './pages/contact-pages/contact-pages.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+    HomePagesComponent,
+    AboutPagesComponent,
+    SidebarComponent,
+    ContactPagesComponent,
+  ],
+  imports: [CommonModule, RouterModule],
+  exports: [
+    HomePagesComponent,
+    AboutPagesComponent,
+    ContactPagesComponent,
+    SidebarComponent,
+  ],
 })
-export class SharedModule { }
+export class SharedModule {}
